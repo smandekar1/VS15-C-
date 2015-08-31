ba4e00671b5a9aa6800af7259a109f691de6165b
smandekar1 <smandekar1@yahoo.com>
2015-08-31 13:26:08 -0500

    Switch Case scenario

--- Program.cs
+++ Program.cs
@@ -10,8 +10,40 @@ namespace MyFirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is a new app");
+            int a = 45;
+            int b = 34;
+
+            Console.WriteLine("1. Add 2.  Sub 3.  Mul 4.  Div ");
+            int ch = int.Parse(Console.ReadLine());
+
+            switch (ch)
+            {
+
+                case 1:
+                    Console.WriteLine(a + b);
+                    break;
+
+                case 2:
+                    Console.WriteLine(a - b);
+                    break;
+
+
+                case 3:
+                    Console.WriteLine(a * b);
+                    break;
+
+
+                case 4:
+                    Console.WriteLine((double)a / b);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+
             Console.ReadLine();
+
         }
     }
 }
