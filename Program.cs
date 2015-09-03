23dc0bb955294e402e165dd5129517c2a560c8d3
smandekar1 <smandekar1@yahoo.com>
2015-09-02 20:32:47 -0500

    using different namspaces

--- Program.cs
+++ Program.cs
@@ -3,101 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeamB.GroupA;
 
-namespace MyFirstApp
+//namespace TeamA
+//{
+
+//    class Student
+//    {
+//        public void Display()
+//        {
+
+//        }
+//    }
+//}
+namespace TeamB
 {
-    class Customer
+    class Student
     {
-        int AcNo;
-        string Name;
-        double Bal;
-        string Contact;
-
-
-        public Customer()
+        public void Show()
         {
-            Console.WriteLine("Hello Wolrd");
-        }
-
-
-        public Customer(int AcNo, string Name, double Bal):this()
-        {
-            this.AcNo = AcNo;
-            this.Name = Name;
-            this.Bal = Bal;
-           
 
         }
-
-
-        public Customer(int AcNo, string Name, double Bal, string Contact) : this(AcNo, Name, Bal)
-        {
-            //this.AcNo = AcNo;
-            //this.Name = Name;
-            //this.Bal = Bal;
-            this.Contact = Contact;
-
-        }
-
-        public void Deposit(double amt)
-        {
-            Bal = Bal + amt;
-        }
-
-        public void Withdraw(double amt)
-        {
-            Bal = Bal - amt;
-        }
-
-
-
-        public void BalEnq()
-        {
-            Console.WriteLine("AcNo:{0}Name:{1} Bal: {2} Contact: {3}", AcNo, Name, Bal, Contact);
-        }
-
-        public void BalEnq(double amt, string f)
-        {
-            if(f=="D")
-            {
-                Bal += amt;
-            }
-            else if(f=="W")
-            {
-                Bal -= amt;
-
-            }
-            else
-            {
-                Console.WriteLine("Invalid Flag");
-            }
-            Console.WriteLine("AcNo:{0}Name:{1} Bal: {2} Contact: {3}", AcNo, Name, Bal, Contact);
-
-        }
-
     }
-        
-    class Program
+
+    class Teacher
     {
-        static void Main(string[] args)
+        public void GetSalary()
         {
-            Customer c = new Customer(1234,"Jack", 78000, "9898565");
+
+        }
+    }
+
+    namespace GroupA
+    {
+        class Test
+        {
+            public void Go()
+            {
+                Console.WriteLine("This is a go method ");
+            }
+        }
+    }
+
+    namespace MyFirstApp
+    {
+        class Program
+        {
+
+            static void Main(string[] args)
+            {
+                //TeamA.Student s = new TeamA.Student();
+                //TeamB.Student s1 = new TeamB.Student();
+                //s.Display();
+                //s1.Show();
+
+                //TeamB.GroupA.Test t = new TeamB.GroupA.Test();
+
+                Test t = new Test();
+                t.Go();
+                Console.ReadLine();
 
 
-            //c.CreateCustomer(1234, "Jack", 78000);
-
-            //c.BalEnq();
-
-            //c.CreateCustomer(1234, "Jack", 78000);
-
-            c.BalEnq();
-
-            Customer c1 = new Customer(123, "Peter", 89000);
-
-            c1.BalEnq();
-
-
-            Console.ReadLine();
+            }
         }
     }
 }
